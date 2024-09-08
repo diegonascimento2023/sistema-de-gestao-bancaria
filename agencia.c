814c89a40c34505b2f6e3f4010890e01f6f0f12a
Diego Nascimento de Oliveira <138056676+diegonascimento2023@users.noreply.github.com>
2024-09-08 13:38:06 +0000

    :sparkles: feat: Criação da função criar_agencia

--- agencia.c
+++ agencia.c
@@ -7,3 +7,23 @@ struct agenciabancaria
     Lista *contas;
     char horario[15];
 };
+
+
+Agenciabancaria *criar_agencia(char *nome, int codigo, char *localizacao, char *horario)
+{
+
+    Agenciabancaria *criar_agencia = (Agenciabancaria *)malloc(sizeof(Agenciabancaria));
+    if (criar_agencia == NULL)
+    {
+
+        printf("Variavel criar_agencia nao foi alocada dinamicamente! \n");
+        exit(1);
+    }
+    strcpy(criar_agencia->nome, nome);
+    criar_agencia->codigo = codigo;
+    strcpy(criar_agencia->localizacao, localizacao);
+    strcpy(criar_agencia->horario, horario);
+    criar_agencia->contas = NULL;
+
+    return criar_agencia;
+}
